1331718fc44a74bcf4fac139352d0b43b5512083
aryasakshi13 <sakshiarya884@gmail.com>
2026-03-25 15:57:51 +0530

    done with callback hell and pyramid of doom

--- Callback.js
+++ Callback.js
@@ -1,3 +1,20 @@
-setTimeout( function (){
-    console.log("callback is running")
-},5000)
+//  normal callback  function
+// setTimeout( function (){
+//     console.log("callback is running")
+// },5000)
+
+// const cart = []
+
+//  Callback Hell 
+ const cart = ["shoes","paints", "kurta"];
+
+ api.createOrder(cart, function (){
+    api.addCart(function (){
+        api.proceedTopayment(function (){
+            api.showOrderSummary(function (){
+
+            })
+        })
+    })
+ })
+
--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # CAllback FUnction 
 function which pass as a argument in another function 
 
+# CAllback Hell 
+One callback inside another callback inside another callback is known as callback hell and the code start to grows horizontlly instead of vertically and this structure is known  as pyramid of doom . This type of code very unmaintainable.
+ 
